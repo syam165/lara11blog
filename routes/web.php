f3faf40536de6cd6cf404237b3437428401deefc
Syamsul Arief <syam165@gmail.com>
2024-06-27 16:49:20 +0700

    Commit ke-3

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', ['title' => 'Homepage']);
+    return view('home', ['title' => 'Home']);
 });
 
 Route::get('/posts', function () {
