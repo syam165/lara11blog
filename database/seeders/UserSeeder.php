cdc68c57a9df9905bdfce67a7ee71417de0d2b0a
Syamsul Arief <syam165@gmail.com>
2024-08-06 10:35:51 +0700

    edit config

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,9 +17,9 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'name' => 'Arebos',
-            'username' => 'arebos',
-            'email' => 'arebos@gmail.com',
+            'name' => 'Arevo Dihan',
+            'username' => 'arevo',
+            'email' => 'indobloger@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'remember_token' =>Str::random(10)
